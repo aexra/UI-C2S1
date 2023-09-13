5cf4040aab52e25c296381695ec1a24df3d556de
Никита Фомин <defdXs@yandex.ru>
2023-09-13 17:52:06 +0300

    Funny preheader title

--- Website/js/fixed-header.js
+++ Website/js/fixed-header.js
@@ -1,26 +1,38 @@
 window.onscroll = function() {myFunction()};
 
+var preheader = document.getElementById("preheader");
+var preh = preheader.offsetHeight;
+
+var prehtit = document.getElementById("preh-title");
+var style = window.getComputedStyle(prehtit, null).getPropertyValue('font-size');
+const default_prehtit_size = parseFloat(style); 
+
 var header = document.getElementById("header");
 var h = header.offsetHeight;
-var sticky = header.offsetTop;
+var headertop = header.offsetTop;
 
-var sfh = document.getElementById("sfh");
-var lastSfhPos = sfh.offsetTop;
+// var sfh = document.getElementById("sfh");
+// var lastSfhPos = sfh.offsetTop;
 
 var content = document.getElementsByClassName("content")[0];
 
 function myFunction() {
-  if (window.pageYOffset > sticky) {
+  // stick the header
+  if (window.pageYOffset > headertop) {
     content.style.paddingTop = h;
     header.classList.add("sticky");
   } else {
     content.style.paddingTop = 0;
     header.classList.remove("sticky");
   }
-  if (sfh.offsetTop > lastSfhPos) {
-    sfh.height = 0;
+  
+  // resize preheader bk
+  var d = preh - window.pageYOffset;
+  if (d <= 0) {
+    preheader.style.backgroundSize = "100px 100px";
   } else {
-    sfh.height = 28;
+    var scale = d / preh * 100;
+    preheader.style.backgroundSize = `${scale+100}% ${scale+100}%`;
+    if (!(scale < 70)) prehtit.style.fontSize = `${default_prehtit_size / 100 * (scale)}px`;
   }
-  lastSfhPos = sfh.offsetTop;
 }
