4da828c459c91f74f4d090d89525910a09630193
Никита Фомин <defdXs@yandex.ru>
2023-10-16 22:49:53 +0300

    Ok maybe we have some progress

---
+++ Website/js/catalog.js
@@ -0,0 +1,26 @@
+const circles = document.getElementsByClassName("outer-circle");
+
+var flags = new Map();
+for (let i = 0; i < circles.length; i++) {
+	flags.set(circles[i], false);
+	circles[i].onclick = onCircleClicked;
+}
+
+function disable(target) {
+	target.style.backgroundColor = "#262932";
+	flags[target] = false;
+}
+
+function enable(target) {
+	target.style.backgroundColor = "#fff";
+	flags[target] = true;
+}
+
+function onCircleClicked(e) {
+	// выключим все кроме него
+	for (let i = 0; i < circles.length; i++) {
+		disable(circles[i]);
+	}
+	// включим нужный
+	enable(e.target);
+}
