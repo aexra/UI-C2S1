42b81e148f975ea6b2719ef5e7aaa33583c00198
Никита Фомин <defdXs@yandex.ru>
2023-10-21 21:41:56 +0300

    Sounds lol

---
+++ Game/js/test.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+	var hover = new Audio("../resources/main/music/hover.ogg");
+	hover.volume = 0.2;
+
+	var hover_extra = new Audio("../resources/main/music/hover-extra.ogg");
+	hover_extra.volume = 0.2;
+
+	for (let btn of document.getElementsByClassName("main-menu-button")) {
+		btn.onmouseenter = (e) => {
+			hover.play();
+		}
+	}
+
+	document.getElementById("main-menu-icon").onmouseenter = (e) => {
+		hover_extra.play();
+	}
+}
