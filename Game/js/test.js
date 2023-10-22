249bffbddac26c5920cbab4757a97fb548ffd605
Никита Фомин <defdXs@yandex.ru>
2023-10-22 12:11:23 +0300

    Click sound

--- Game/js/test.js
+++ Game/js/test.js
@@ -1,14 +1,20 @@
+var hover = new Audio("../resources/main/music/hover.ogg");
+hover.volume = 0.2;
+
+var hover_extra = new Audio("../resources/main/music/hover-extra.ogg");
+hover_extra.volume = 0.2;
+
+var click = new Audio("../resources/main/music/click.ogg");
+click.volume = 0.1;
+
 window.onload = function() {
-	var hover = new Audio("../resources/main/music/hover.ogg");
-	hover.volume = 0.2;
-
-	var hover_extra = new Audio("../resources/main/music/hover-extra.ogg");
-	hover_extra.volume = 0.2;
-
 	for (let btn of document.getElementsByClassName("main-menu-button")) {
 		btn.onmouseenter = (e) => {
 			hover.play();
 		}
+		btn.addEventListener("click", (e) => {
+			click.play();
+		});
 	}
 
 	document.getElementById("main-menu-icon").onmouseenter = (e) => {
