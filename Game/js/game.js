8274586cfa52c6b974b7422c2f82cf7313cedde2
Nikita Fomin <defdXs@yandex.ru>
2023-10-25 14:17:25 +0300

    Styled banner

--- Game/js/game.js
+++ Game/js/game.js
@@ -5,9 +5,15 @@ setLevelBanner(level);
 function setLevelBanner(level) {
 	document.getElementById("level-banner").classList.add("lvl" + level);
 	document.getElementById("level-banner-title").innerHTML = level == 1? "EASY" : level == 2? "MEDIUM" : level == 3? "HARD" : "GODMODE";
-	document.getElementById("level-banner-svg").src = `../resources/main/artwork/lvl${level}.svg`;
-
+	
+	for (image of document.getElementsByClassName("level-banner-svg")) {
+		image.src = `../resources/main/artwork/lvl${level}.svg`;
+	}
 	for (corner of document.getElementsByClassName("level-banner-corner")) {
 		corner.classList.add("lvl" + level);
+
+		if (level == 4) {
+			corner.style.boxShadow = "0 0 30px gray";
+		}
 	}
 }
