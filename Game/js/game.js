b988af42b4eb371a738ccc4571c1f8fcaf58074b
Nikita Fomin <defdXs@yandex.ru>
2023-10-25 14:04:47 +0300

    Added content to level banner

--- Game/js/game.js
+++ Game/js/game.js
@@ -4,6 +4,9 @@ setLevelBanner(level);
 
 function setLevelBanner(level) {
 	document.getElementById("level-banner").classList.add("lvl" + level);
+	document.getElementById("level-banner-title").innerHTML = level == 1? "EASY" : level == 2? "MEDIUM" : level == 3? "HARD" : "GODMODE";
+	document.getElementById("level-banner-svg").src = `../resources/main/artwork/lvl${level}.svg`;
+
 	for (corner of document.getElementsByClassName("level-banner-corner")) {
 		corner.classList.add("lvl" + level);
 	}
