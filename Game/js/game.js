5df405f5223d1825a71897776c67d7a938775f62
Nikita Fomin <defdXs@yandex.ru>
2023-10-25 13:48:29 +0300

    Added level banner on game page

--- Game/js/game.js
+++ Game/js/game.js
@@ -1,1 +1,10 @@
 const level = sessionStorage.getItem("diff")[3]; // this is int (1..4)
+
+setLevelBanner(level);
+
+function setLevelBanner(level) {
+	document.getElementById("level-banner").classList.add("lvl" + level);
+	for (corner of document.getElementsByClassName("level-banner-corner")) {
+		corner.classList.add("lvl" + level);
+	}
+}
