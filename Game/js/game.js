be39f081a039b7d03435b324aad21a66efddc0f1
Никита Фомин <defdXs@yandex.ru>
2023-11-11 17:12:50 +0300

    Now game.js is a module

--- Game/js/game.js
+++ Game/js/game.js
@@ -1,15 +1,16 @@
-const level = sessionStorage.getItem("diff")[3]; // this is int (1..4)
-
-setLevelBanner(level);
+window.onload = (e) => {
+	const level = sessionStorage.getItem("diff")[3]; // this is int (1..4)
+	setLevelBanner(level);
+};
 
 function setLevelBanner(level) {
 	document.getElementById("level-banner").classList.add("lvl" + level);
 	document.getElementById("level-banner-title").innerHTML = level == 1? "EASY" : level == 2? "MEDIUM" : level == 3? "HARD" : "GODMODE";
-	
-	for (image of document.getElementsByClassName("level-banner-svg")) {
+
+	for (let image of document.getElementsByClassName("level-banner-svg")) {
 		image.src = `../resources/main/artwork/lvl${level}.svg`;
 	}
-	for (corner of document.getElementsByClassName("level-banner-corner")) {
+	for (let corner of document.getElementsByClassName("level-banner-corner")) {
 		corner.classList.add("lvl" + level);
 
 		if (level == 4) {
