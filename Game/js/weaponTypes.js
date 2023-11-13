105e385dfc20fe847b18b97842e633bd697fdc28
Nikita Fomin <defdXs@yandex.ru>
2023-11-13 11:26:19 +0300

    Input works well

--- Game/js/weaponTypes.js
+++ Game/js/weaponTypes.js
@@ -3,6 +3,15 @@ import { Weapon } from "./weapon.js"
 export class MeleeWeapon extends Weapon {
     constructor(player) {
         super(player);
+
+        this.range = 50;
+        this.isAttacking = false;
+        this.attackingFrame = 0;
+    }
+    update(input, deltaTime) {
+        if (input.includes("lmb")) {
+            console.log("ATTACK!!!");
+        }
     }
 }
 
