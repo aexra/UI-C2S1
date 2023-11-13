369986d5486c704531665cbf470612f79f2a9f7b
Nikita Fomin <defdXs@yandex.ru>
2023-11-13 11:21:31 +0300

    Added overrided constructors

--- Game/js/weaponTypes.js
+++ Game/js/weaponTypes.js
@@ -1,17 +1,25 @@
 import { Weapon } from "./weapon.js"
 
 export class MeleeWeapon extends Weapon {
-
+    constructor(player) {
+        super(player);
+    }
 }
 
 export class RangedWeapon extends Weapon {
-
+    constructor(player) {
+        super(player);
+    }
 }
 
 export class MagicWeapon extends Weapon {
-
+    constructor(player) {
+        super(player);
+    }
 }
 
 export class SummonerWeapon extends Weapon {
-
+    constructor(player) {
+        super(player);
+    }
 }
