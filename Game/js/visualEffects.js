2f4c11ee039234edd76c9722517fc8c17d6aad3e
Никита Фомин <defdXs@yandex.ru>
2023-12-04 11:20:07 +0300

    Prepared to make this *cool* effect

--- Game/js/visualEffects.js
+++ Game/js/visualEffects.js
@@ -8,9 +8,24 @@ export class DraedonInitiatingBeam extends VisualEffect {
         
         this.position = this.game.map.codebreaker.position.copy();
         this.size = new Vec2(100, 100);
+
+        this.keyframes = [800, 1000, 2500, 3000];
     }
     update(input, deltaTime) {
         this.updateLifeTimer(input, deltaTime);
+
+        if (this.lifetimer <= this.keyframes[0]) {
+
+        }
+        else if (this.lifetimer <= this.keyframes[1]) {
+
+        }
+        else if (this.lifetimer <= this.keyframes[2]) {
+
+        }
+        else if (this.lifetimer <= this.keyframes[3]) {
+            
+        }
     }
     draw(c) {
         c.fillStyle = "red";
