bc628ba68825065201d7f06d3a1106d2f7a4b926
Никита Фомин <defdXs@yandex.ru>
2023-12-10 15:42:37 +0300

    Boss arc drawing

--- Game/js/bossHpIndicator.js
+++ Game/js/bossHpIndicator.js
@@ -7,17 +7,17 @@ export class BossHPIndicator extends GameObject {
         this.ui = ui;
         this.npc = npc;
 
-        this.indicatorSize = new Vec2(300, 60);
-        this.progressSize = new Vec2(100, 40);
-        this.progressPosition = new Vec2(200, -40);
+        this.indicatorSize = new Vec2(300, 50);
+        this.progressSize = new Vec2(160, 40);
+        this.progressPosition = new Vec2(140, -40);
 
-        this.onUIPosition = new Vec2(ui.size.x - this.indicatorSize.x, ui.size.y - this.indicatorSize.y);
+        this.onUIPosition = new Vec2(ui.size.x - this.indicatorSize.x - 60, ui.size.y - this.indicatorSize.y);
     }
     update(input, deltaTime) {
         this.position = new Vec2(this.ui.position.x + this.onUIPosition.x / this.ui.game.player.camera.scale.x, -this.ui.position.y + this.onUIPosition.y / this.ui.game.player.camera.scale.y);
         this.size = new Vec2(this.indicatorSize.x / this.ui.game.player.camera.scale.x, this.indicatorSize.y / this.ui.game.player.camera.scale.y);
-        this.progressSize = new Vec2(100 / this.ui.game.player.camera.scale.x, 40 / this.ui.game.player.camera.scale.y);
-        this.progressPosition = new Vec2(200 / this.ui.game.player.camera.scale.x, -40 / this.ui.game.player.camera.scale.y);
+        this.progressSize = new Vec2(160 / this.ui.game.player.camera.scale.x, 40 / this.ui.game.player.camera.scale.y);
+        this.progressPosition = new Vec2(140 / this.ui.game.player.camera.scale.x, -40 / this.ui.game.player.camera.scale.y);
     }
     draw(c) {
         this.drawProgressBar(c);
@@ -29,10 +29,31 @@ export class BossHPIndicator extends GameObject {
         c.fillRect(this.position.x, this.position.y, this.size.x, this.size.y);
     }
     drawPercentageBar(c) {
-        c.fillstyle = "yellow";
+        // c.fillstyle = "yellow";
+        // c.fillRect(this.position.x + this.progressPosition.x, this.position.y + this.progressPosition.y, this.progressSize.x, this.progressSize.y);
+        c.fillStyle = "rgb(48, 28, 8)";
         c.fillRect(this.position.x + this.progressPosition.x, this.position.y + this.progressPosition.y, this.progressSize.x, this.progressSize.y);
+        c.strokeStyle = "rgb(71, 45, 13)";
+        c.lineWidth = 5;
+        c.strokeRect(this.position.x + this.progressPosition.x, this.position.y + this.progressPosition.y, this.progressSize.x, this.progressSize.y);
     }
     drawBossArc(c) {
+        c.fillStyle = "rgb(40, 2, 17)";
+        c.strokeStyle = "rgb(71, 45, 13)";
+        c.lineWidth = 5;
 
+        c.beginPath();
+        c.arc(this.position.x + this.size.x, this.position.y, 50, 0, 2 * Math.PI);
+        c.fill();
+        
+        c.beginPath();
+        c.arc(this.position.x + this.size.x, this.position.y, 50, 0, 2 * Math.PI);
+        c.stroke();
+
+        c.strokeStyle = "rgb(196, 160, 99)";
+        c.lineWidth = 3;
+        c.beginPath();
+        c.arc(this.position.x + this.size.x, this.position.y, 50, 0, 2 * Math.PI);
+        c.stroke();
     }
 }
